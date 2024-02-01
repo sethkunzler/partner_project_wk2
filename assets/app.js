ced4bc515b5bbac2e4f8770a8a54513d66889081
Seth Kunzler <sethkunzler@gmail.com>
2024-02-01 14:47:35 -0700

    seth taking over

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,56 @@
-console.log('onnected');
+const heroes = [
+{
+  name: "Jerms",
+  emoji: "🦞",
+  hp: 15, 
+  skill: 4,
+  cost: 0,
+  level: 1
+},
+
+{
+  name: "Mick",
+  hp: 30,
+  skill: 1,
+  cost: 0,
+  level: 1
+},
+
+{
+  name: "Paul Bunyan",
+  hp: 20,
+  skill: 8,
+  cost: 300,
+  level: 3
+},
+
+{
+  name: "Paul Revere",
+  hp: 25,
+  skill: 5,
+  cost: 500,
+  level: 4
+},
+{
+  name: "Jake",
+  hp: 200,
+  skill: 30,
+  cost: 1000,
+  level: 10
+}
+]
+
+const boss = {
+  name: "Tony Hawk",
+  hp: 30,
+  skill: 2,
+  gold: 50,
+  level: 1
+}
+
+
+
+
+function attackBoss() {
+
+}
